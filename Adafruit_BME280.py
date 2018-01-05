7c5cab5cf08cfb54c5a4a57b1120761523c6af51
pomeroythomas <pomeroythomas@hotmail.com>
2018-01-05 16:52:22 -0700

    Magnus Dew Point Algorithm
    Changed the dew-point approximation to use the Magnus algorithm to facilitate accuracy across the spectrum of relative humidity.

--- Adafruit_BME280.py
+++ Adafruit_BME280.py
@@ -24,6 +24,7 @@
 # THE SOFTWARE.
 import logging
 import time
+import math
 
 
 # BME280 default address.
@@ -89,6 +90,7 @@ BME280_REGISTER_DATA = 0xF7
 
 
 class BME280(object):
+    
     def __init__(self, t_mode=BME280_OSAMPLE_1, p_mode=BME280_OSAMPLE_1, h_mode=BME280_OSAMPLE_1,
                  standby=BME280_STANDBY_250, filter=BME280_FILTER_off, address=BME280_I2CADDR, i2c=None,
                  **kwargs):
@@ -264,16 +266,19 @@ class BME280(object):
         pascals = self.read_pressure()
         inches = pascals * 0.0002953
         return inches
-
+    
     def read_dewpoint(self):
-        # Return calculated dewpoint in C, only accurate at > 50% RH
+        # Return calculated dewpoint in C using the Magnus formula
+        a = 17.271
+        b = 237.7
         celsius = self.read_temperature()
         humidity = self.read_humidity()
-        dewpoint = celsius - ((100 - humidity) / 5)
+        gamma = (a * celsius /(b + celsius)) + math.log(humidity/100.0)
+        dewpoint = (b * gamma) / (a - gamma)
         return dewpoint
-
+        
     def read_dewpoint_f(self):
-        # Return calculated dewpoint in F, only accurate at > 50% RH
+        # Return calculated dewpoint in F using the Magnus formula
         dewpoint_c = self.read_dewpoint()
         dewpoint_f = dewpoint_c * 1.8 + 32
         return dewpoint_f
